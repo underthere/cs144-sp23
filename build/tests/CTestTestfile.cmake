57f99dd9b7acec8b36f3b01f790b0334104a8086
underthere <cltobecl@outlook.com>
2023-12-04 16:59:11 +0800

    Revert "finnish lab2"
    This reverts commit bc97625c9f0a46bf7ba9666dd0d1764853954ae5.

--- build/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/underthere/repos/cs144-sp23/minnow/tests
-# Build directory: /home/underthere/repos/cs144-sp23/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
